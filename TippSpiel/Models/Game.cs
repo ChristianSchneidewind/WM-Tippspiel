bb914c539fafc2d91e287a12d27ce4f49eb1e021
ChristianSchneidewind <gizmovienna@gmail.com>
2026-03-05 13:16:54 +0100

    Anlegen des Models Game

---
+++ TippSpiel/Models/Game.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace TippSpiel.Models
+{
+    public class Game
+        {
+            public int Id{get; set;}
+            [Required]
+            public string HomeTeam {get; set;} = string.Empty;
+            [Required]
+            public string AwayTeam {get; set;} = string.Empty;
+            public DateTime KickOff {get; set;}
+            public int HomeTeamScore {get; set;}
+            public int AwayTeamScore{get; set;}
+
+            public int GroupId {get; set;}
+            public Group? Group {get; set;}
+            
+
+        }
+}
+    
